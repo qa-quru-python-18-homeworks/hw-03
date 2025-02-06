adc0dc8f853bb5cdbc309f73af62ad4d3976ff45
Waldemar G <vladdigen@icloud.com>
2025-02-06 11:12:21 +0400

    Add initial test setup with fixtures for DuckDuckGo search functionality

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+import random
+import string
+from selene import browser
+
+
+@pytest.fixture(scope="session")
+def maximized_browser():
+    browser.driver.maximize_window()
+    yield
+    browser.quit()
+
+
+@pytest.fixture
+def duck_duck_go_page():
+    browser.open("https://duckduckgo.com")
+
+
+@pytest.fixture
+def not_too_long_random_chars():
+    return "".join(random.choices(string.digits, k=500))
+
+
+@pytest.fixture
+def too_long_random_chars():
+    return "".join(random.choices(string.digits, k=1000))
---
+++ test_duckduckgo_search.py
@@ -0,0 +1,26 @@
+from selene import browser, be, have
+
+
+def test_successful_search(maximized_browser, duck_duck_go_page):
+    browser.element('//input[@aria-label="Search with DuckDuckGo"]').should(
+        be.blank
+    ).type("Python").press_enter()
+    browser.element("html").should(have.text("Python.org"))
+
+
+def test_empty_search_results(
+    maximized_browser, duck_duck_go_page, not_too_long_random_chars
+):
+    browser.element('//input[@aria-label="Search with DuckDuckGo"]').should(
+        be.blank
+    ).type(not_too_long_random_chars).press_enter()
+    browser.element("html").should(have.text("No results found"))
+
+
+def test_too_long_search(maximized_browser, duck_duck_go_page, too_long_random_chars):
+    browser.element('//input[@aria-label="Search with DuckDuckGo"]').should(
+        be.blank
+    ).type(too_long_random_chars).press_enter()
+    browser.element("html").should(
+        have.text("Search query entered was too long. Please shorten and try again")
+    )
